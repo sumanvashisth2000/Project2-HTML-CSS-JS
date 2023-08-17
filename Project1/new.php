8c4a26d2f02fbfa84273217c6b2c1c2591856ae8
sumanvashisth <45479082+sumanvashisth2000@users.noreply.github.com>
2023-08-17 22:39:17 +0530

    First Commit

---
+++ Project1/new.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Project</title>
+</head>
+<body>
+    <h1> Learn PHP </h1>
+    <h2> How to Install Basic Environment for using PHP </h2>
+    <h3> How to make a table</h3>
+    <ol> 
+        <li>Window</li>
+        <li>MacOS</li>
+        <li>Linux</li>
+
+    </ol>
+    <h3>Code Editor</h3>
+    <ul>
+        <li>VS Code</li>
+        <li>Atom</li>
+        <li>Notepad</li>
+    </ul>
+</body>
+</html>
